189c5a96b5f2795a41f56c81481971fdce31f518
Carrie Beesley <carrie@carriebeesley.com>
2018-04-14 11:23:23 +0200

    create the initial markup for the two main components

--- test/src/components/Starred.js
+++ test/src/components/Starred.js
@@ -1,13 +1,38 @@
 //@flow
 import React, { Component } from 'react';
 
-class Starred extends Component {
+class StarredInfluencer extends Component {
+  render() {
+    return (
+      <div>
+        <img></img>
+        <h2 className="suggestedInfName">Hannah Ross</h2>
+        <div className="instaHandle"><img className="instaIcon"></img>hross1234</div>
+        <div>12345 Followers</div>
+        <div>6.24% Engagement</div>
+        <button className="removeButton" onClick={() => {}}>X</button>
+      </div>
+    )
+  }
+}
 
+class Starred extends Component {
     render() {
         return (
-            <div className="Starred">
-                Starred
-            </div>
+          <div className="suggested">
+              <h1 className="suggestedHeader">Starred Influencers</h1>
+              <select className="sortFilters">
+                <option value="engagement hilo">Engagement (High - Low)</option>
+                <option value="engagement lohi">Engagement (Low - High)</option>
+                <option value="followers hilo">Followers (High - Low)</option>
+                <option value="followers lohi">Followers (Low - High)</option>
+                <option value="name hilo">Name (A-Z)</option>
+                <option value="name lohi">Instagram Username (A-Z)</option>
+              </select>
+              <div className="suggestedList">
+                <StarredInfluencer/>
+              </div>
+          </div>
         );
     }
 }
--- test/src/components/Suggested.js
+++ test/src/components/Suggested.js
@@ -1,11 +1,27 @@
 //@flow
 import React, { Component } from 'react';
 
+class SuggestedInfluencer extends Component {
+  render() {
+    return (
+      <div>
+        <img></img>
+        <h2 className="suggestedInfName">Hannah Ross</h2>
+        <span className="instaHandle"><img className="instaIcon"></img>hross1234</span>
+        <button className="addButton" onClick={() => {}}>+</button>
+      </div>
+    )
+  }
+}
+
 class Suggested extends Component {
     render() {
         return (
-            <div className="Suggested">
-        Suggested
+            <div className="suggested">
+                <h1 className="suggestedHeader">Suggested Influencers</h1>
+                <div className="suggestedList">
+                  <SuggestedInfluencer/>
+                </div>
             </div>
         );
     }
