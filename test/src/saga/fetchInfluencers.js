9d90d724833a353a921baecb603e2c3e2e7f619c
Carrie Beesley <carrie@carriebeesley.com>
2018-04-15 11:03:09 +0200

    mock out a basic saga - wip

--- test/src/saga/fetchInfluencers.js
+++ test/src/saga/fetchInfluencers.js
@@ -1,7 +1,19 @@
 //@flow
+import { put } from 'redux-saga/effects'
 
+const flattenResponse = () => {
+    //this will be a function to flatten out the data into the model demonstrated in the initialState
+}
+
+/**
+ * 
+ */
 function* fetchInfluencers(action: Object): Iterator<*> {
-    // function to get the starred and suggested influencers
+    const starredResponse = yield fetch('path-to-starred-influencers');
+    const starredJson = flattenResponse(yield starredResponse.json());
+    const suggestedResponse = yield fetch('path-to-suggested-influencers');
+    const suggestedJson = flattenResponse(yield suggestedResponse.json());
+    yield put('INFLUENCER_FETCH_SUCEEDED');
 }
 
 export default fetchInfluencers;
