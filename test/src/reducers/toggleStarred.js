0df297560fe68b8d64ab11842937039b018b8ec8
Carrie Beesley <carrie@carriebeesley.com>
2018-04-16 22:38:21 +0200

    last bugfix

--- test/src/reducers/toggleStarred.js
+++ test/src/reducers/toggleStarred.js
@@ -1,18 +1,16 @@
 //@flow
 
 /**
- * toggleStarred - toggles the 'starred' field on the selected influencer
- *
- * @param  {Array}  state   The application state
- * @param  {Object} action  The redux action dispatched
- * @return {Array}          The new or unmodified state
- */
-const toggleStarred = (state: Array = [], action: Object): Object => {
-    if (action.type === 'TOGGLE_STARRED') {
-        const influencerIndex = state.findIndex((influencer: Object): number => influencer.id === action.id);
-        if (influencerIndex > -1) {
-            Object.assign(state[influencerIndex], {starred: !(state[influencerIndex].starred)});
-        }
+* toggleStarred - toggles the 'starred' field on the selected influencer
+*
+* @param  {Array}  state   The application state
+* @param  {Object} action  The redux action dispatched
+* @return {Array}          The new or unmodified state
+*/
+const toggleStarred = (state: Array = [], action: Object): Array => {
+    const influencerIndex = state.findIndex((influencer: Object): number => influencer.id === action.id);
+    if (influencerIndex > -1) {
+        Object.assign(state[influencerIndex], {starred: !(state[influencerIndex].starred)});
     }
     return state;
 };
